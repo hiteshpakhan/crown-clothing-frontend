5bebc9fe9993b5a066292d79f67f6f906f48778e
Hitesh Pakhan <hitesh5pakhan@gmail.com>
2022-05-03 17:36:43 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+// import { Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+// const HatsPage = () => (
+//   <div>
+//     <h1>HATS PAGE</h1>
+//   </div>
+// );
 
 function App() {
   return (
     <div className="App">
-      {/* <HomePage /> */}
-      <Route exact path="/" component={HomePage} />
+      <HomePage />
+      {/* <Route exact path="/" component={HomePage} /> */}
     </div>
   );
 }
