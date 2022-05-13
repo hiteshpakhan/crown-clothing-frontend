b8fd261a40ad02e3d4d1f3651afdbb15a74d82d6
Hitesh Pakhan <hitesh5pakhan@gmail.com>
2022-05-13 17:24:27 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';   // importing the Route that support the routing
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
-// const HatsPage = () => (
-//   <div>
-//     <h1>HATS PAGE</h1>
-//   </div>
-// );
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <Route exact path='/' component={HomePage} />
+      <Switch >  {/* Switch only render the component that has the exact path */}
+        <Route path='/' component={HomePage} />  {/* here the component homepage will apper on the / path ,and the exact have the values of true or false  */}
+        <Route path='/hats' component={HatsPage} />    
+      </Switch>
     </div>
   );
 }
